a7238befe66cbb959327262ce4304c7b3346e0ba
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2022-04-02 11:01:45 -0400

    global error messages yay \o/

--- www/app.js
+++ www/app.js
@@ -2,6 +2,14 @@
 window.ws_url = 'wss://'+location.hostname+':'+location.port+'/ws'
 window.socket = false;
 
+// Development cheating
+window.onerror = function(message, source, lineno, colno, error) {
+  window.socket.send(
+    // Ensures a "safe" string for browsers to eval
+    JSON.stringify('message='+message+' source='+source+' lineno='+lineno+' error='+error)
+  );
+};
+
 function setup_ws() {
   if (!window.socket) {
     console.log('Connecting to '+window.ws_url)
@@ -21,5 +29,70 @@ function setup_ws() {
   }
 }
 
-setInterval(setup_ws,6000);
+setInterval(setup_ws, 6000);
 setup_ws();
+
+// GUI utils
+
+function checkSupportedState_immersive_ar() {
+  return new Promise((resolve, reject) => {
+    navigator.xr.isSessionSupported('immersive-ar').then((supported) => {
+      if (supported) {
+        window.immersive_ar_supported = true;
+        resolve();
+      }
+      else{
+        window.immersive_ar_supported = false;
+      }
+    })
+  });
+}
+
+function checkSupportedState_immersive_vr() {
+  return new Promise((resolve, reject) => {
+    navigator.xr.isSessionSupported('immersive-vr').then((supported) => {
+      if (supported) {
+        window.immersive_vr_supported = true;
+        resolve();
+      }
+      else{
+        window.immersive_vr_supported = false;
+      }
+    })
+  });
+}
+
+function render_immersive_vr() {
+  document.getElementById('experience_target').innerText = 'Loading Immersive VR!';
+}
+function render_immersive_ar() {
+  document.getElementById('experience_target').innerText = 'Loading Immersive AR!'; 
+}
+function render_basic() {
+  document.getElementById('experience_target').innerText = 'Loading Basic!'; 
+}
+
+// GUI drivers
+function render_app() {
+  checkSupportedState_immersive_vr();
+  checkSupportedState_immersive_ar();
+
+  if (window.immersive_vr_supported) {
+    render_immersive_vr();
+  }
+  else if (window.immersive_ar_supported) {
+    render_immersive_ar();
+  }
+  else {
+    render_basic();
+  }
+
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  checkSupportedState_immersive_vr();
+  checkSupportedState_immersive_ar();
+  setTimeout(() => {
+    render_app();
+  }, 800)
+})
